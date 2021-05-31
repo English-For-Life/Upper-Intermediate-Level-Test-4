951a9c24018114285d14d9d19b14a1096ff1efe9
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-31 08:52:35 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I could have explained in more detail, but that's the problem in a ..... .",
+        options:["nutshell", "short", "cheese", "gist"],
+        answer: 0
+    },
+    {
+        q:"Fortunately, I ..... finish the report in time.",
+        options:["could", "managed to", "was able", "can have"],
+        answer: 1
+    },
+    {
+        q:"I'm not a big fan of his films. There is too much violence that's over-the-..... .",
+        options:["moon", "hill", "top", "summit"],
+        answer: 2
+    },
+    {
+        q:"I'm afraid the rent prices can soar here. This area is up-and- ..... .",
+        options:["going", "selling", "booming", "coming"],
+        answer: 3
+    },
+    {
+        q:"People are really struggling because wages are so low there and the ..... of living is so high.",
+        options:["cost", "price", "standard", "quality"],
+        answer: 0
+    },
+    {
+        q:"He came off the track. He was trying to ..... on a corner and lost control of a car.",
+        options:["undergo", "overtake", "undertake", "overgo"],
+        answer: 1
+    },
+    {
+     q:"After the ..... yesterday it was almost impossible to open the front door.",
+        options:["monsoon", "hail", "blizzard", "drought"],
+        answer: 2   
+    },
+    {
+        q:"The governor had to apologize for his ..... remarks about the homeless.",
+        options:["illegal", "irrational", "unemotional", "insensitive"],
+        answer: 3
+    },
+    {
+        q:"There have been multiple reports of identity ..... this month as scams are becoming more elaborate.",
+        options:["theft", "thief", "thieve", "steal"],
+        answer: 0
+    },
+    {
+        q:"He is a very deep sleeper. He could easily sleep ..... an earthquake!",
+        options:["on", "through", "over", "in"],
+        answer: 1
+    }
+]
